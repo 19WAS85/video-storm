00fa06b5ec77dc96085cd80d90e02ef8fe7fc987
Wagner Andrade <19WAS85+github@gmail.com>
2020-06-25 14:58:58 -0300

    Include express

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const server = require('http').createServer()
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+
+const PORT = 3160
+const WEB_PORT = PORT + 1
 
 io.on('connection', (socket) => {
   console.log('Conected')
   socket.on('disconnect', () => console.log('Disconnected'))
 })
 
-server.listen(3160)
+app.use(express.static('public'))
+
+server.listen(PORT, () => console.log(`Socket at http://localhost:${PORT}`))
+app.listen(WEB_PORT, () => console.log(`App at http://localhost:${WEB_PORT}`))
